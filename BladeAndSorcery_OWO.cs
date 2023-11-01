32c44e50e8f45baa38df443e2aa99f93a63ba515
floh-bhaptics <floh@fahrenbergers.de>
2023-11-01 07:55:54 +0100

    Add project files.

---
+++ BladeAndSorcery_OWO.cs
@@ -0,0 +1,38 @@
+﻿using MelonLoader;
+using MyOWOVest;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using ThunderRoad;
+
+namespace BladeAndSorcery_OWO
+{
+    public class BladeAndSorcery_OWO : MelonMod
+    {
+        public static TactsuitVR tactsuitVr;
+
+
+        public override void OnInitializeMelon()
+        {
+            tactsuitVr = new TactsuitVR();
+
+        }
+
+        [HarmonyPatch(typeof(SpellCaster), "Fire", new Type[] { })]
+        public class bhaptics_CastFire
+        {
+            [HarmonyPostfix]
+            public static void Postfix(SpellCaster __instance)
+            {
+                bool isRight = (__instance.side == Side.Right);
+                bool twoHanded = (__instance.isMerging);
+                tactsuitVr.CastSpell(isRight, twoHanded);
+            }
+        }
+
+    }
+}
